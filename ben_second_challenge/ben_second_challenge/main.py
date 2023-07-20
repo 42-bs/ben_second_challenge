da1ed11f4780493620fc128a96a3495d50d7e39c
Juliana Milson <67876847+julianamilson@users.noreply.github.com>
2023-07-20 15:43:20 -0300

    Add files via upload
    mds

---
+++ ben_second_challenge/ben_second_challenge/main.py
@@ -0,0 +1,15 @@
+from srcs.data_point_kafka_producer import DataPointKafkaProducer
+import kafka
+import kafka.errors
+from kafka import KafkaConsumer, TopicPartition
+import time
+import os
+
+if __name__ == "__main__":
+	serverhost = os.getenv("KAFKA_HOST")
+	print(serverhost)
+	producer = DataPointKafkaProducer(server=serverhost, retry_limit=50)
+	while (True):
+		print("sending")
+		producer.send_through_kafka()
+		time.sleep(2)
---
+++ ben_second_challenge/ben_second_challenge/srcs/data_point_builder.py
@@ -0,0 +1,53 @@
+import random
+import string
+from datetime import datetime
+
+class DataPointBuilder:
+	"""
+	This class maintain the configuration, rules
+	and restrictions about every variable of the DataPoint object.
+	"""
+
+	def __init__(self,
+		id_list_size = 10,
+		long_max = 9223372036854775807,
+		max_energy_value = 9999999,
+		characters_allowed = string.ascii_letters + string.digits):
+		"""
+		Args:
+		"""
+
+		self._id_list_size = id_list_size
+		self._long_max = long_max
+		self._max_energy_value = max_energy_value
+		self._id_list = [random.randrange(1, long_max) for _ in range(id_list_size)]
+
+	def id(self):
+		"""
+		Choose a random id from the list of ids generated on construction.
+
+		Returns:
+			long: a random id as long
+		"""
+
+		return random.choice(self._id_list)
+
+	def value(self):
+		"""
+		Create a random double value to represent the value field.
+
+		Returns:
+			double: a random value as double
+		"""
+
+		return round(random.uniform(1, self._max_energy_value), 2)
+
+	def date(self):
+		"""
+		Create a random timestamp value to represent the date field.
+
+		Returns:
+			date: a random timestamp value as date
+		"""
+
+		return datetime.now(datetime.timezone.utc)
