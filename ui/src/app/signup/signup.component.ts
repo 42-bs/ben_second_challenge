d76e06a7723cd27bc64de223cb206a3cde134f1f
renanguillen <rigcpj@gmail.com>
2023-07-26 15:22:51 -0300

    angular frontend init

---
+++ ui/src/app/signup/signup.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.sass']
+})
+export class SignupComponent {
+    username: string = "";
+    password: string = "";
+    repassword: string = "";
+    show: boolean = false;
+    submit() {
+        console.log("user name is " + this.username)
+        this.clear();
+    }
+    clear() {
+        this.username = "";
+        this.password = "";
+        this.show = true;
+    }
+    sendauthdata() {
+        
+        this.http.post<any>("http://localhost:3000/signupUsersList",this.)
+        .subscribe(res=>{alert('SIGNIN SUCCESFUL');
+        this.router.navigate(["login"])},
+        err=>{alert("Something went wrong")})
+    }
+}
